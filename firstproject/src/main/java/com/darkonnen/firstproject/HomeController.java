251d46c1cf36591aa2f55ed5eaead1e7af87e2a8
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-05-27 22:54:28 -0400

    finished date excercise

--- firstproject/src/main/java/com/darkonnen/firstproject/HomeController.java
+++ firstproject/src/main/java/com/darkonnen/firstproject/HomeController.java
@@ -1,38 +1,43 @@
 package com.darkonnen.firstproject;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 //@RequestMapping("/hello")
+@Controller
 public class HomeController {
-
-//	@RequestMapping("")
 	@RequestMapping("/")
-	public String index(@RequestParam(value = "q", required = false) String searchQuery) {
-		if (searchQuery == null) {
-			return "You searched nothing";
-		} else {
-//		return "Hewaaalao World";
-			return String.format("You searched for: %s", searchQuery);
-		}
+	public String home(Model model) {
+		model.addAttribute("dojoName", "burbank");
+		return "index.jsp";
 	}
-//	localhost:8080/?q=myQueryParam
-	
-//	Query Paramas vs Path Variables
-
-    @RequestMapping("/m/{track}/{module}/{lesson}")
-    public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson){
-    	return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;
-    }
-//    http://localhost:8080/m/123/456/7890
-
-
-//	@RequestMapping("/world")
-//	public String world() {
-//		return "Class level anotations";
-//	}
 
 }
+////	@RequestMapping("")
+//	@RequestMapping("/")
+//	public String index(@RequestParam(value = "q", required = false) String searchQuery) {
+//		if (searchQuery == null) {
+//			return "You searched nothing";
+//		} else {
+////		return "Hewaaalao World";
+//			return String.format("You searched for: %s", searchQuery);
+//		}
+//	}
+////	localhost:8080/?q=myQueryParam
+//	
+////	Query Paramas vs Path Variables
+//
+//    @RequestMapping("/m/{track}/{module}/{lesson}")
+//    public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson){
+//    	return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;
+//    }
+////    http://localhost:8080/m/123/456/7890
+//
+//
+////	@RequestMapping("/world")
+////	public String world() {
+////		return "Class level anotations";
+////	}
+
